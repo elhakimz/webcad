def9010c9ace5a22c81bd33e7b1f334d60d79a0e
Abiel <el.hakimz@gmail.com>
2026-05-11 18:08:02 +0700

    Make gizmo rotation rings thicker for visibility

--- src/render/GizmoRenderer.ts
+++ src/render/GizmoRenderer.ts
@@ -27,7 +27,7 @@ export class GizmoRenderer {
   private readonly ARROW_HEAD_RAD = 1.5;
   private readonly SHAFT_RAD = 0.3;
   private readonly RING_RADIUS = 55;
-  private readonly RING_TUBE_RAD = 0.15;
+  private readonly RING_TUBE_RAD = 0.5;
   private readonly RING_SEGMENTS = 64;
   private readonly HANDLE_OPACITY = 0.85;
   private readonly HIGHLIGHT_COLOR = 0xddc040;
